71937f48351935e262063ed5caa55193b490f637
unknown <subash.prabaharan@infiligence.com>
2026-08-12 12:22:30 +0530

    login-screenshots-done

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,34 +1,41 @@
+import screenshot from 'screenshot-desktop';
+
 class loginPage{
 
-    constructor(page){
-
+     constructor(page) {
     this.page = page;
     this.credentials = page.locator('//button[normalize-space()="Continue with Credentials"]');
     this.usernameInput = page.locator('#username');
     this.passwordInput = page.locator('#password');
     this.loginButton = page.getByRole('button', { name: 'Sign In' });
-    this.selOrg = page.getByRole('combobox').filter({ hasText: 'Select Organization' });
-    this.opt = page.getByRole('option', { name: 'auragxp (agxp)' });
+    this.selOrg = page.locator('button[role="combobox"]');
+    this.opt = page.getByRole('option', { name: 'auratrace' });
     this.proceed = page.getByRole('button', { name: 'Proceed' });
-    }
+  }
 
     async goto(){
-        await this.page.goto('https://auragxp-uat.auratrace.ai/');
+        await this.page.goto('https://dev-ph2.auratrace.ai/');
         await this.page.waitForLoadState('networkidle');
+        await screenshot({ filename: './screenshots/login-page.jpg',fullpage: true });
 
     }
 
 
-        async login(username, password){
-            await this.credentials.click();
-            await this.usernameInput.fill(username);
-            await this.passwordInput.fill(password);
-            await this.loginButton.click();
-            // await this.selOrg.click();
-            // await this.opt.click();
-            // await this.proceed.click();
-        }
+     async login(username, password) {
+    await this.credentials.click();
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await screenshot({ filename: './screenshots/cred-page.jpg',fullpage: true });
+    await this.loginButton.click();
+    await this.selOrg.click();
+    await this.opt.click();
+    await this.proceed.click();
+    await this.page.waitForTimeout(5000)
+    await screenshot({filename : './screenshots/home-page.jpg',fullpage:true});
+    
 
+
+  }
 
 
 }
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,18 +3,7 @@ import loginPage from '../pages/loginPage.js';
 import screenshot from 'screenshot-desktop';
 
 test('@login Login Test', async ({ page }) => {
-    // await page.setViewportSize({
-    //     width: 1920,
-    //     height: 1080
-    // });
-
     const login = new loginPage(page);
-
     await login.goto();
-    await screenshot({ filename: './screenshots/login-page.jpg',fullpage: true });
-
-    await login.login('admin@auragxp.com', 'admin@12345');
-    await page.waitForTimeout(5000);
-    await screenshot({ filename: './screenshots/cred-page.jpg',fullpage: true });
-
+    await login.login('admin@auratrace.ai', 'admin');
 });
