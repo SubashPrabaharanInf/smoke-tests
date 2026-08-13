81acc7acc709d554abb535da22b8ef522cc7e3bd
unknown <subash.prabaharan@infiligence.com>
2026-08-13 11:52:53 +0530

    lms-dashboard

--- pages/lms.js
+++ pages/lms.js
@@ -1,19 +1,18 @@
-import screenshotDesktop from "screenshot-desktop";
-
-
+import screenshot from "screenshot-desktop";
 
 class lms {
 
     constructor(page){
     this.page = page;
     this.lmbtn = '//span[text()="Learning"]';
-    this.myLrn = `//div//div//a[text()='My Learning Dashboard']`;
+    this.myLrn = `//div//div//a[text()='Course Management']`;
  }
 
   async learningDashboard() {
     await this.page.locator(this.lmbtn).click();
     await this.page.locator(this.myLrn).click();
-    await screenshot({ filename: './screenshots-lms/lmsDashboard-page.jpg',fullpage: true });
+    await this.page.waitForURL('https://dev-ph2.auratrace.ai/lms/courses');
+    await screenshot({ filename: './screenshots/coursemanagement-page.jpg',fullpage: true });
     
 
 
@@ -21,3 +20,5 @@ class lms {
 
  
 }
+
+export default lms;
--- tests/lms.spec.js
+++ tests/lms.spec.js
@@ -1,10 +1,12 @@
-import {test} from playwright/test;
-import { lms } from '../pages/lms.js';
+import {test} from "@playwright/test";
+import LMS from '../pages/lms.js';
+import loginPage from "../pages/loginPage.js";
 
 
 test("@lms LMS dashboard",async({page})=>{
-    const lms = new lms(page);
+   
     const login = new loginPage(page);
+    const lms = new LMS(page);
     await login.goto();
     await login.login('admin@auratrace.ai', 'admin');
     await lms.learningDashboard();
